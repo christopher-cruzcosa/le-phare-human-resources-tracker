01e238bf1b788f13134389bc0dec0295d4b92b8c
Chris C <christopher.cruzcosa@gmail.com>
2020-10-06 20:02:31 -0700

    Added final functionality and styling to templates

--- app.js
+++ app.js
@@ -4,154 +4,146 @@ const Intern = require("./lib/intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-const employeeArray = [];
-
-
-function main_prompt() {
-    return inquirer.prompt([
-        {
-
-            type: "list",
-            message: "A new employee record is being created. What type of employee record are you entering?",
-            name: "role",
-            choices: [
-                "Manager",
-                "Engineer",
-                "Intern",
-            ]
-        },
-        {
-            type: "input",
-            message: "What is the employee's name?",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "What is the employee's ID number?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "What is the employee's email address",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "What is the managers's office number?",
-            name: "officeNumber",
-            when: function (answers) {
-                return answers.role === "Manager"
-            },
-        },
-        {
-            type: "input",
-            message: "What is the name of your school?",
-            name: "school",
-            when: function (answers) {
-                return answers.role === "Intern"
-            },
-        },
-        {
-            type: "input",
-            message: "What is your Github username?",
-            name: "github",
-            when: function (answers) {
-                return answers.role === "Engineer"
-            },
-        },
-    ])
-
-};
-
-// function secondary_prompt(answer) {
-//     if (answer.role === "Manager") {
-//         answer.officeNumber = inquirer.prompt([
-//             {
-//                 type: "input",
-//                 message: "What is the managers's office number?",
-//                 name: "officeNumber",
-//             },
-//         ])
-//     } else
-//         if (answer.role === "Intern") {
-//             answer.school = inquirer.prompt([
-//                 {
-//                     type: "input",
-//                     message: "What is the name of your school?",
-//                     name: "school",
-//                 },
-//             ])
-//         } else
-//             if (answer.role === "Engineer") {
-//                 answer.github = inquirer.prompt([
-//                     {
-//                         type: "input",
-//                         message: "What is your Github username?",
-//                         name: "github",
-//                     },
-//                 ])
-//             }
-//     return answer
-// }
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
-function employeeCreation() {
-    main_prompt()
-        .then((answers) => {
-            if (answers.role === "Manager") {
-                const newEmployee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-                employeeArray.push(newEmployee);
-                console.log("New employee record created");
-                console.log(employeeArray);
-            }
-            if (answers.role === "Intern") {
-                const newEmployee = new Intern(answers.name, answers.id, answers.email, answers.school)
-                employeeArray.push(newEmployee);
-                console.log("New employee record created");
-                console.log(employeeArray);
-            }
-            if (answers.role === "Manager") {
-                const newEmployee = new Engineer(answers.name, answers.id, answers.email, answers.github)
-                employeeArray.push(newEmployee);
-                console.log("New employee record created");
-                console.log(employeeArray);
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-};
+class Team {
+    constructor() {
+        this.staff = [];
+    }
 
-function main_control (){
-    let keepWorking = true;
-    console.log("Welcome to the le Phare Corp's Human Resources digitizer!")
-    // while (keepWorking) {
-    //     employeeCreation();
-    //     if (inquirer.prompt([
-    //         {
-    //             type: "list",
-    //             message: "Do you wish to make another employee record?",
-    //             name: "continue",
-    //             choices: [
-    //                 "yes",
-    //                 "no"
-    //             ]
-    //         },
-    //     ]) === {continue: "no"}) {  
-    //         keepWorking = false;
-    //         console.log("Goodbye, friend")
-    //     }
+    run() {
+        console.log("Welcome to the le Phare Corp's Human Resources digitizer!")
+        return this.addEmployee();
+    }
+
+    addEmployee() {
+        return inquirer
+            .prompt([
+                {
+
+                    type: "list",
+                    message: "A new employee record is being created. What type of employee record are you entering?",
+                    name: "role",
+                    choices: [
+                        "Manager",
+                        "Engineer",
+                        "Intern",
+                    ]
+                },
+                {
+                    type: "input",
+                    message: "What is the employee's name?",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "What is the employee's ID number?",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "What is the employee's email address",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "What is the managers's office number?",
+                    name: "officeNumber",
+                    when: function (answers) {
+                        return answers.role === "Manager"
+                    },
+                },
+                {
+                    type: "input",
+                    message: "What is the name of your school?",
+                    name: "school",
+                    when: function (answers) {
+                        return answers.role === "Intern"
+                    },
+                },
+                {
+                    type: "input",
+                    message: "What is your Github username?",
+                    name: "github",
+                    when: function (answers) {
+                        return answers.role === "Engineer"
+                    },
+                },
+            ])
+            .then((answers) => {
+                if (answers.role === "Manager") {
+                    const newEmployee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+                    this.staff.push(newEmployee);
+                    return this.confirmDone();
+                }
+                if (answers.role === "Intern") {
+                    const newEmployee = new Intern(answers.name, answers.id, answers.email, answers.school)
+                    this.staff.push(newEmployee);
+                    return this.confirmDone();
+                }
+                if (answers.role === "Engineer") {
+                    const newEmployee = new Engineer(answers.name, answers.id, answers.email, answers.github)
+                    this.staff.push(newEmployee);
+                    return this.confirmDone();
+                }
+                return this.confirmDone();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    confirmDone() {
+        return inquirer
+            .prompt([
+                {
+                    type: "confirm",
+                    message: "A new employee record was created successfull! Do you want to make another record?",
+                    name: "continue",
+                },
+            ])
+            .then((answers) => {
+                if (answers.continue) {
+                    return this.addEmployee();
+                }
+                return this.renderFile()
+            })
+            // .then((answers) => {
+            //     fs.writeFile(outputPath,answers)
+            // })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    renderFile(){
+        fs.writeFile(outputPath, render(this.staff), (err) => {
+            if (err) throw err;
+            console.log('A new team.html file was created!');
+          })
+    }
+
+    // createHTML() {
+    //     const HTMLtext = render(this.staff);
+
+    //     fs.writeFile(outputPath,HTMLtext);
+    //     // this.staff.forEach((task, i) => console.log(`${i + 1}. ${task}`));
+    //     process.exit(0);
     // }
-    employeeCreation();
-};
+}
 
-main_control();
+const newTeam = new Team();
+
+newTeam.run();
+
+module.exports = Team;
 
 
 
