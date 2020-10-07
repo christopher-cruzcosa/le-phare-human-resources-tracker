38e772bdc6ef147a687ee41793b61735eee48a0f
Chris C <christopher.cruzcosa@gmail.com>
2020-10-06 21:29:46 -0700

    Added notations and final styling choices

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # le-phare-human-resources-tracker
+A human resources tracker for team leaders to organize their personnel information.
+
+## Installation
+
+This web app is made up oen central app.js file and five supplmental js files, Inquirer and Jest modules, four template html files, and images for screenshots and the company logo.
+
+The location of the Github repo is: https://github.com/christopher-cruzcosa/le-phare-human-resources-tracker - this is not currently deployed live.
+
+## Usage 
+
+Click here for a video walkthrough: https://youtu.be/oc1sOLnE83o
+
+-----
+
+Below is screenshot of the CLI to generate the tracker.
+
+![Screenshot 1](./lib/images/screenshot1.png)
+
+-----
+
+Below is screenshot of a finished html output as rendered in a browser.
+
+![Screenshot 2](./lib/images/screenshot2.png)
+
+
+## Credits
+
+Created by: Christopher Cruzcosa
+
+
+## License
+
+Copyright (c) [2020] [Christopher Cruzcosa]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- app.js
+++ app.js
@@ -10,19 +10,20 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const writeFileAsync = util.promisify(fs.writeFile);
 
-
+//This class controls the prompts and eventually generates the team.html file from the entered employees
 class Team {
     constructor() {
         this.staff = [];
     }
 
+    //This function begins the data collection
     run() {
         console.log("Welcome to the le Phare Corp's Human Resources digitizer!")
         return this.addEmployee();
     }
 
+    //This function gathers the information on a new employee
     addEmployee() {
         return inquirer
             .prompt([
@@ -62,7 +63,7 @@ class Team {
                 },
                 {
                     type: "input",
-                    message: "What is the name of your school?",
+                    message: "What is the name of the intern's school?",
                     name: "school",
                     when: function (answers) {
                         return answers.role === "Intern"
@@ -70,13 +71,15 @@ class Team {
                 },
                 {
                     type: "input",
-                    message: "What is your Github username?",
+                    message: "What is the engineer's Github username?",
                     name: "github",
                     when: function (answers) {
                         return answers.role === "Engineer"
                     },
                 },
             ])
+            //This function uses the data to create a new class object, either manager or intern or engineer
+            //Also, it gives the user the chance to make another employee or end the data collection
             .then((answers) => {
                 if (answers.role === "Manager") {
                     const newEmployee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
@@ -100,6 +103,7 @@ class Team {
             });
     }
 
+    //This function asks the user if they want to continue
     confirmDone() {
         return inquirer
             .prompt([
@@ -115,57 +119,26 @@ class Team {
                 }
                 return this.renderFile()
             })
-            // .then((answers) => {
-            //     fs.writeFile(outputPath,answers)
-            // })
             .catch((err) => {
                 console.log(err);
             });
     }
 
+    //This function generates the team.html file
     renderFile(){
         fs.writeFile(outputPath, render(this.staff), (err) => {
             if (err) throw err;
             console.log('A new team.html file was created!');
           })
     }
-
-    // createHTML() {
-    //     const HTMLtext = render(this.staff);
-
-    //     fs.writeFile(outputPath,HTMLtext);
-    //     // this.staff.forEach((task, i) => console.log(`${i + 1}. ${task}`));
-    //     process.exit(0);
-    // }
 }
 
+
+//creating a new Team object
 const newTeam = new Team();
 
+//activating the team object to begin the application
 newTeam.run();
 
 module.exports = Team;
 
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
